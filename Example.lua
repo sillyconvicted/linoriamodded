ba9a05be54898e6dbef1aa2eab32f50963394efa
sillyconvicted <130371349+sillyconvicted@users.noreply.github.com>
2025-06-08 19:56:13 +0300

    removed invalid import

--- Example.lua
+++ Example.lua
@@ -4,7 +4,6 @@
 local repo = 'https://raw.githubusercontent.com/sillyconvicted/linoriamodded/main/'
 
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
-local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
 local Window = Library:CreateWindow({
