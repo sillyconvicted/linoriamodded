94d902bf189a02f3391eef29107e052de117d569
sillyconvicted <130371349+sillyconvicted@users.noreply.github.com>
2025-06-08 19:47:30 +0300

    removed theme manager

--- Example.lua
+++ Example.lua
@@ -22,7 +22,7 @@ local Window = Library:CreateWindow({
     
 
     Colors = {
-        AccentColor = Color3.fromRGB(0, 255, 140),      -- Bright green accent
+        AccentColor = Color3.fromRGB(255, 255, 255),      -- Bright green accent
         -- MainColor = Color3.fromRGB(35, 35, 35),         -- Slightly lighter main color
         -- BackgroundColor = Color3.fromRGB(25, 25, 25),   -- Darker background
         -- FontColor = Color3.fromRGB(255, 255, 255),      -- White text
@@ -438,7 +438,6 @@ Library.ToggleKeybind = Options.MenuKeybind -- Allows you to have a custom keybi
 -- ThemeManager (Allows you to have a menu theme system)
 
 -- Hand the library over to our managers
-ThemeManager:SetLibrary(Library)
 SaveManager:SetLibrary(Library)
 
 -- Ignore keys that are used by ThemeManager.
@@ -452,15 +451,10 @@ SaveManager:SetIgnoreIndexes({ 'MenuKeybind' })
 -- use case for doing it this way:
 -- a script hub could have themes in a global folder
 -- and game configs in a separate folder per game
-ThemeManager:SetFolder('MyScriptHub')
 SaveManager:SetFolder('MyScriptHub/specific-game')
 
 -- Builds our config menu on the right side of our tab
 SaveManager:BuildConfigSection(Tabs['UI Settings'])
-
--- Builds our theme menu (with plenty of built in themes) on the left side
--- NOTE: you can also call ThemeManager:ApplyToGroupbox to add it to a specific groupbox
-ThemeManager:ApplyToTab(Tabs['UI Settings'])
 
 -- You can use the SaveManager:LoadAutoloadConfig() to load a config
 -- which has been marked to be one that auto loads!
